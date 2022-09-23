003ca5e1c40292a25076f5c97d22c72b0972cf83
Dilbar <dibux9@gmail.com>
2022-09-23 12:10:50 -0400

    spring-labs

--- lab-04-springmvc/src/main/java/com/cydeo/lab04springmvc/controller/ProductController.java
+++ lab-04-springmvc/src/main/java/com/cydeo/lab04springmvc/controller/ProductController.java
@@ -1,25 +1,24 @@
 package com.cydeo.lab04springmvc.controller;
 
-import com.cydeo.lab04springmvc.model.Product;
 import com.cydeo.lab04springmvc.service.ProductService;
-import com.cydeo.lab04springmvc.service.impl.ProductServiceImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.List;
 
 @Controller
 public class ProductController {
 
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
     @RequestMapping("search-product/{name}")
     public String searchProduct(@PathVariable String name, Model model){
 
-        ProductService productService = new ProductServiceImpl();
-        List<Product> list = productService.searchProduct(name);
-        model.addAttribute("productList", list);
+        model.addAttribute("productList", productService.searchProduct(name));
 
         return "product/product-list";
     }
