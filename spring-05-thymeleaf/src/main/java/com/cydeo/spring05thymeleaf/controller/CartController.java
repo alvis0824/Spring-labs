1ee3eaf983c396fe3463f8033d9fd76e40386cad
Dilbar <dibux9@gmail.com>
2022-09-29 10:23:23 -0400

    fixed errors

--- spring-05-thymeleaf/src/main/java/com/cydeo/spring05thymeleaf/controller/CartController.java
+++ spring-05-thymeleaf/src/main/java/com/cydeo/spring05thymeleaf/controller/CartController.java
@@ -4,9 +4,7 @@ import com.cydeo.spring05thymeleaf.service.impl.CartServiceImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.UUID;
 
